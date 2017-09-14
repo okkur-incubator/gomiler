52f34a4a7e9af4b68a0be18dfba823bfb954e8d1
Muhammad Adeel Hussain <AdeilH@users.noreply.github.com>
2017-09-14 20:17:10 +0500

    Made the changes.
    Changed the code for the increment of page to work. Please check it.

--- daily_milestone.go
+++ daily_milestone.go
@@ -1,29 +1,121 @@
 package main
 
-import "flag"
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
 
-func get_project_id(base_url string, token string, projectname string, namespace string) string {
-	headers := map[string]string{"PrivateToken": token}
-	url := "https://" + base_url + "/projects"
-	page := 1
-	for {
-
-	}
-
+// Struct to be used for milestone
+// Milestone ....
+type gitLabAPI struct {
+	ID          int               `json:"id"`
+	Iid         int               `json:"iid"`
+	ProjectID   int               `json:"project_id"`
+	Title       string            `json:"title"`
+	Description string            `json:"description"`
+	StartDate   string            `json:"start_date"`
+	DueDate     string            `json:"due_date"`
+	State       string            `json:"state"`
+	UpdatedAt   *time.Time        `json:"updated_at"`
+	CreatedAt   *time.Time        `json:"created_at"`
+	Name        string            `json:"name"`
+	NameSpace   map[string]string `json:"namespace"`
 }
 
-//func get_milestones(base_url string, token string, project_id string)[]string{}
-//func create_milestone_data(advance int) []string{}
-//func create_milestones(base_url string, token string, project_id string, milestones string) string{}
+// Initialization of logging variable
+var logger *log.Logger
+
+// LoggerSetup Initialization
+func LoggerSetup(info io.Writer) {
+	logger = log.New(info, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+// Function to get project ID from the gitLabAPI
+func getProjectID(baseURL string, Token string, Projectname string, Namespace string) string {
+	project := gitLabAPI{}
+	urls := "https://" + baseURL + "/projects"
+	page := 1
+
+	for {
+
+		strPage := strconv.Itoa(page)
+		s := []string{urls, "?page=", strPage}
+		completeURL := strings.Join(s, "")
+		client := &http.Client{}
+		req, err := http.NewRequest("GET", completeURL, nil)
+		if err != nil {
+			logger.Println(err)
+			break
+		}
+		req.Header.Add("Token", Token)
+		resp, err := client.Do(req)
+		if err != nil {
+			logger.Println(err)
+			break
+		}
+		respByte, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			logger.Println("fail to read response data")
+			break
+		}
+		json.Unmarshal(respByte, project)
+		defer resp.Body.Close()
+		fmt.Println(resp.Body)
+		fmt.Println(project)
+		if project.Name == "message" {
+			fmt.Println(project.Name)
+		}
+		if project.Name == Projectname && project.NameSpace["path"] == Namespace {
+			return strconv.Itoa(project.ID)
+		}
+		if project.Title == "" {
+			break
+		}
+		page++
+	}
+	return strconv.Itoa(project.ID)
+}
+
+// CreateMilestoneData is used to check the due date using the time package of python
+func CreateMilestoneData(advance int) []string {
+	today := time.Now().Local()
+	list := []string{}
+	for i := 0; i < advance; i++ {
+		date := today.AddDate(0, 0, i)
+		dateiso := date.Format("2009-01-02")                            // To Format to ISOFormat and it converts to string so can be used in list directly
+		datelist := []string{"Title: ", "dateiso", "due_date", dateiso} // Was unable to get a map in a list so made this
+		y := strings.Join(datelist, ",")
+		list = append(list, y)
+
+	}
+	return list
+}
 
 func main() {
-	var apiKey, apiBase, namespace, project string
-	var advance int
-	flag.StringVar(&apiKey, "token", " ", "Gitlab api key/token.")
-	flag.StringVar(&apiBase, "base_url", " ", "Gitlab api base url")
-	flag.StringVar(&namespace, "namespace", " ", "Namespace to use in Gitlab")
-	flag.StringVar(&project, "projectname", " ", "Project to use in Gitlab")
-	flag.IntVar(&advance, "Advance", 30, "Define timeframe to generate milestones in advance.")
-	flag.Parse()
+	// Declaring variables for flags
+	var Token, APIBase, Namespace, Project string
+	var Advance int
+	APIBase = "lol"
+	// Command Line Parsing Starts
+	flag.StringVar(&Token, "Token", "lol", "Gitlab api key/token.")
+	flag.StringVar(&APIBase, "baseURL", " ", "Gitlab api base url")
+	flag.StringVar(&Namespace, "Namespace", " ", "Namespace to use in Gitlab")
+	flag.StringVar(&Project, "ProjectName", " ", "Project to use in Gitlab")
+	flag.IntVar(&Advance, "Advance", 30, "Define timeframe to generate milestones in advance.")
+	flag.Parse() //Command Line Parsing Ends
 
+	// Initializing logger
+	LoggerSetup(os.Stdout)
+	// Calling getProjectID
+	getProjectID(APIBase, Token, Project, Namespace)
+	CreateMilestoneData(Advance)
 }
